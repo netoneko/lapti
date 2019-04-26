5fbcca1a9e639f6ecab4af9ae54b2dba561c89cd
Kirill Maksimov <kirill@orbs.com>
2019-04-26 23:28:26 +0300

    List WIP

--- contract/contract.go
+++ contract/contract.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-	"strconv"
-
+	"encoding/hex"
 	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1"
+	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1/address"
 	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1/state"
+	"strconv"
 )
 
-var PUBLIC = sdk.Export(saveRevision, getLastRevision)
+var PUBLIC = sdk.Export()
 var SYSTEM = sdk.Export(_init)
 
 type Revision struct {
-	ID          uint64
-	Timestamp   uint64
+	ID        uint64
+	Timestamp uint64
 
-	Author      string
-	Name string
-	Text     string
+	Author string
+	Name   string
+	Text   string
 }
 
 func _init() {
@@ -34,16 +35,20 @@ func saveRevision(name string, text string) (messageID uint64) {
 
 func getLastRevision(name string) Revision {
 	revisionId := lastRevisionId(name)
+	return getRevisionById(revisionId)
+}
+
+func getRevisionById(revisionId uint64) Revision {
 	return Revision{
-		ID: revisionId,
-		Name: name,
-		Text: state.ReadString(textKey(revisionId)),
+		ID:     revisionId,
+		Author: hex.EncodeToString(state.ReadBytes(authorKey(revisionId))),
+		Name:   state.ReadString(nameKey(revisionId)),
+		Text:   state.ReadString(textKey(revisionId)),
 	}
 }
 
-
-func key(revisionId uint64, postfix string) []byte {
-	return []byte(strconv.FormatUint(revisionId, 10) + "_" + postfix)
+func getRevisions(name string) []Revision {
+	return nil
 }
 
 func newRevision(name string) (revisionId uint64) {
@@ -52,14 +57,62 @@ func newRevision(name string) (revisionId uint64) {
 
 	// Save latest revision by that name
 	state.WriteUint64([]byte(name), revisionId)
+	state.WriteString(nameKey(revisionId), name)
+	state.WriteBytes(authorKey(revisionId), address.GetCallerAddress())
 
 	return
+}
+
+func key(revisionId uint64, postfix string) []byte {
+	return []byte(strconv.FormatUint(revisionId, 10) + "_" + postfix)
 }
 
 func textKey(revisionId uint64) []byte {
 	return key(revisionId, "t")
 }
 
+func nameKey(revisionId uint64) []byte {
+	return key(revisionId, "n")
+}
+
 func lastRevisionId(name string) (revisionId uint64) {
 	return state.ReadUint64([]byte(name))
+}
+
+func authorKey(revisionId uint64) []byte {
+	return key(revisionId, "a")
+}
+
+type ListSerializer func(id uint64, params ...interface{})
+type ListDeserializer func(id uint64) interface{}
+
+type List interface {
+	Count() uint64
+	Add(...interface{}) uint64
+	Get(id uint64) interface{}
+}
+
+type list struct {
+	prefix       string
+	serializer   ListSerializer
+	deserializer ListDeserializer
+}
+
+func NewList(prefix string, serializer ListSerializer, deserializer ListDeserializer) List {
+	return list{prefix, serializer, deserializer}
+}
+
+func (l list) Count() uint64 {
+	return state.ReadUint64([]byte(l.prefix + "_counter"))
+}
+
+func (l list) Add(params ...interface{}) uint64 {
+	count := l.Count() + 1
+	state.WriteUint64([]byte(l.prefix+"_counter"), count)
+	l.serializer(count, params...)
+	return count
+}
+
+func (l list) Get(id uint64) interface{} {
+	return l.deserializer(id)
 }
--- contract/contract_test.go
+++ contract/contract_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/hex"
+	"fmt"
+	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1/state"
 	. "github.com/orbs-network/orbs-contract-sdk/go/testing/unit"
 	"github.com/stretchr/testify/require"
 	"testing"
@@ -14,8 +17,10 @@ func Test_saveRevision_Single(t *testing.T) {
 
 		bowieFirstRevision := getLastRevision("David Bowie")
 
+		require.EqualValues(t, 1, bowieFirstRevision.ID)
 		require.EqualValues(t, "David Bowie", bowieFirstRevision.Name)
 		require.EqualValues(t, "The greatest performer of the 20th century", bowieFirstRevision.Text)
+		require.EqualValues(t, hex.EncodeToString(caller), bowieFirstRevision.Author)
 	})
 }
 
@@ -27,15 +32,55 @@ func Test_saveRevision_Multiple(t *testing.T) {
 
 		bowieFirstRevision := getLastRevision("David Bowie")
 
+		require.EqualValues(t, 1, bowieFirstRevision.ID)
 		require.EqualValues(t, "David Bowie", bowieFirstRevision.Name)
 		require.EqualValues(t, "The greatest performer of the 20th century", bowieFirstRevision.Text)
-
+		require.EqualValues(t, hex.EncodeToString(caller), bowieFirstRevision.Author)
 
 		saveRevision("Iggy Pop", "Another great performer of the 20th century")
 
 		iggyFirstRevision := getLastRevision("Iggy Pop")
 
+		require.EqualValues(t, 2, iggyFirstRevision.ID)
 		require.EqualValues(t, "Iggy Pop", iggyFirstRevision.Name)
 		require.EqualValues(t, "Another great performer of the 20th century", iggyFirstRevision.Text)
+		require.EqualValues(t, hex.EncodeToString(caller), iggyFirstRevision.Author)
+	})
+}
+
+func Test_listRevisions(t *testing.T) {
+	t.Skip()
+	caller := AnAddress()
+
+	InServiceScope(nil, caller, func(m Mockery) {
+		saveRevision("David Bowie", "The greatest performer of the 20th century")
+		saveRevision("David Bowie", "The singer of the 20th century")
+
+		revisions := getRevisions("David Bowie")
+		require.Equal(t, 1, revisions[0].ID)
+		require.Equal(t, 2, revisions[0].ID)
+	})
+}
+
+func TestList(t *testing.T) {
+	caller := AnAddress()
+
+	InServiceScope(nil, caller, func(m Mockery) {
+		s := func(id uint64, params ...interface{}) {
+			state.WriteString([]byte(fmt.Sprintf("h_%d", id)), params[0].(string))
+		}
+
+		d := func(id uint64) interface{} {
+			return state.ReadString([]byte(fmt.Sprintf("h_%d", id)))
+		}
+
+		l := NewList("some_list", s, d)
+		l.Add("hello!")
+
+		require.EqualValues(t, 1, l.Count())
+
+		item := l.Get(1)
+
+		require.EqualValues(t, "hello!", item)
 	})
 }
